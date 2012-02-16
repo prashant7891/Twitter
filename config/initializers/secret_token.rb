7b310aa1786b56b672712d7dc68f28a998a36010
Giri Prashant <max.payne7891@gmail.com>
2012-02-16 22:56:53 +0530

    Username and micropost done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = '228f6e71eb348b69054c5a34b184d36baa5a8eb712771e686c28740cf9dd4df55a022385adee9f3053e2981abd11a0a170d2e4daa7ac097af1363ae595330269'
